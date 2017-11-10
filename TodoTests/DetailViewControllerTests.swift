6e86ec959b76016b9df1b7879465b3587cb6ea50
Martin Kelly <marty.kelly2009@gmail.com>
2017-11-10 11:55:06 +0000

    added more tests to detailViewController

--- TodoTests/DetailViewControllerTests.swift
+++ TodoTests/DetailViewControllerTests.swift
@@ -13,12 +13,14 @@ import CoreLocation
 class DetailViewControllerTests: XCTestCase {
     
     var sut: DetailViewController!
+    var itemManager: ItemManager!
     
     override func setUp() {
         super.setUp()
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         sut = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        itemManager = ItemManager()
         sut.loadViewIfNeeded()
     }
     
@@ -32,15 +34,9 @@ class DetailViewControllerTests: XCTestCase {
         XCTAssertTrue(mapViewIsSubView)
     }
     
-    func test_SettingItemInfo_SetsTextsToLabels() {
-        let coordinate = CLLocationCoordinate2DMake(51.2277, 6.7735)
-        let location = Location(name: "Foo", coordinate: coordinate)
-        let item = TodoItem(title: "Bar",
-                            itemDescription: "Baz",
-                            timestamp: 1456150025,
-                            location: location)
+    func test_SettingItemInfo_SetsTitleLabel() {
+        let item = TodoItem(title: "Bar")
         
-        let itemManager = ItemManager()
         itemManager.add(item)
         
         sut.itemInfo = (itemManager, 0)
@@ -48,11 +44,51 @@ class DetailViewControllerTests: XCTestCase {
         sut.beginAppearanceTransition(true, animated: true)
         sut.endAppearanceTransition()
         XCTAssertEqual(sut.titleLabel.text, "Bar")
+    }
+    
+    func test_SettingItemInfo_SetsDateLabel() {
+        let item = TodoItem(title: "Foo", timestamp: 1456150025)
+        itemManager.add(item)
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
         XCTAssertEqual(sut.dateLabel.text, "02/22/2016")
-        XCTAssertEqual(sut.locationLabel.text, "Foo")
+    }
+    
+    func test_SettingItemInfo_SetsDescriptionLabel() {
+        let item = TodoItem(title: "Foo", itemDescription: "Baz")
+        itemManager.add(item)
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
         XCTAssertEqual(sut.descriptionLabel.text, "Baz")
+    }
+    
+    func test_SettingItemInfo_SetsLocationLabel() {
+        let location = Location(name: "Foo")
+        let item = TodoItem(title: "Foo", location: location)
+        
+        itemManager.add(item)
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
+        XCTAssertEqual(sut.locationLabel.text, "Foo")
+    }
+    
+    func test_SettingItemInfo_SetsMapView() {
+        let coordinate = CLLocationCoordinate2DMake(51.2277, 6.7735)
+        let location = Location(name: "Foo", coordinate: coordinate)
+        let item = TodoItem(title: "Foo", location: location)
+        
+        itemManager.add(item)
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
         XCTAssertEqualWithAccuracy(sut.mapView.centerCoordinate.latitude, coordinate.latitude, accuracy: 0.001)
         XCTAssertEqualWithAccuracy(sut.mapView.centerCoordinate.longitude, coordinate.longitude, accuracy: 0.001)
-        
     }
 }
